a17e78e09808b3999cca867a91c9a811e7d0a00c
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-07-24 23:58:22 +0200

    Autocomplete Widget Activity

--- app/src/main/java/com/seif/placesdemo/MainActivity.kt
+++ app/src/main/java/com/seif/placesdemo/MainActivity.kt
@@ -1,14 +1,24 @@
 package com.seif.placesdemo
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.Autocomplete
+import com.google.android.libraries.places.widget.AutocompleteActivity
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
 import com.seif.placesdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 lateinit var binding: ActivityMainBinding
 lateinit var navController: NavController
+    private val AUTOCOMPLETE_REQUEST_CODE = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -16,5 +26,49 @@ lateinit var navController: NavController
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
         navController = navHostFragment.navController
 
+
+
+        // Set the fields to specify which types of place data to
+        // return after the user has made a selection.
+        val fields = listOf(Place.Field.ID, Place.Field.NAME)
+
+        // Start the autocomplete intent.
+        val intent = Autocomplete.IntentBuilder(AutocompleteActivityMode.FULLSCREEN, fields)
+            .build(this)
+        startActivityForResult(intent, AUTOCOMPLETE_REQUEST_CODE)
+
+
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == AUTOCOMPLETE_REQUEST_CODE) {
+            when (resultCode) {
+                Activity.RESULT_OK -> {
+                    data?.let {
+                        val place = Autocomplete.getPlaceFromIntent(data)
+                        Log.i("mainActivity", "Place: ${place.name}, ${place.id}")
+                    }
+                }
+                AutocompleteActivity.RESULT_ERROR -> {
+                    // TODO: Handle the error.
+                    data?.let {
+                        val status = Autocomplete.getStatusFromIntent(data)
+                        Log.i("mainActivity", status.statusMessage ?: "")
+                    }
+                }
+                Activity.RESULT_CANCELED -> {
+                    // The user canceled the operation.
+                }
+            }
+            return
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
--- app/src/main/java/com/seif/placesdemo/Summary.kt
+++ app/src/main/java/com/seif/placesdemo/Summary.kt
@@ -1,6 +1,7 @@
 package com.seif.placesdemo
 
 /** video 11 : Autocomplete Widget Fragment **/
+// Option 1: Embed an AutocompleteSupportFragment
 // The autocomplete service in the Places SDK for Android returns place predictions in response
 // to user search queries. As the user types, the autocomplete service returns suggestions
 // for places such as businesses, addresses, plus codes, and points of interest.
@@ -8,3 +9,16 @@ package com.seif.placesdemo
 // You can add autocomplete to your app in the following ways:
 // - Add an autocomplete widget to save development time and ensure a consistent user experience.
 // - Get place predictions programmatically to create a customized user experience.
+
+/** video 12 : Autocomplete Widget Activity **/
+// Option 2: Use an intent to launch the autocomplete activity (Note: These steps aren't needed if you are embedding a fragment.)
+// If you want your app to use a different navigational flow
+// (for example, to trigger the autocomplete experience from an icon rather than a search field),
+// your app can launch autocomplete by using an intent.
+// Caution: Do NOT launch the AutocompleteActivity directly. Instead, use an intent to launch Autocomplete
+// (PlaceAutocomplete in the compatibility library).
+//
+// To launch the autocomplete widget using an intent, follow these steps:
+// 1- Use Autocomplete.IntentBuilder to create an intent, passing the desired Autocomplete mode.
+// The intent must call startActivityForResult, passing in a request code that identifies the intent.
+// 2- Override the onActivityResult callback to receive the selected place.
